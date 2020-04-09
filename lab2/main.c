68f4d37e370a127cb8a4a2fed903c71ed1555b9b
Diana <dianka23@inbox.ru>
2020-04-09 14:29:22 +1000

    done lab1

---
+++ lab2/main.c
@@ -0,0 +1,14 @@
+#include "task1.c"
+#include "task2.c"
+#include "task3.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+//    task1();
+//    task2();
+    task3();
+    return 0;
+}
