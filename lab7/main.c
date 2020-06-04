aaa2ad70c86e3361a8a18236d5398040ef3d8fa9
Diana <dianka23@inbox.ru>
2020-06-04 15:31:08 +1000

    fix lab7

--- lab7/main.c
+++ lab7/main.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define mask64  0b0010000000100000001000000010000000100000001000000010000000100000
+#define mask32  0b00100000001000000010000000100000
+#define mask16  0b0010000000100000
+#define mask8  0b00100000
 
 short isLetter(const char *str) {
-    if (((int) str >= 65 && (int) str <= 90)||((int) str >= 97 && (int) str <= 122)) {
+    if (((int) str >= 65 && (int) str <= 90) || ((int) str >= 97 && (int) str <= 122)) {
         return 1;
     }
     return 0;
 }
 
 void changeCase(char *str, size_t n) {
-    for (int i = 0; i < n; i++) {
-        if (isLetter((const char *) *(str + i))) {
-            //делаем сдвиг на символ. После смещаем на 6 бит и делаем XOR
-            *(str + i) ^= 1 << 5;
+    size_t length = n;
+    str += n;
+    while (length >= 8) {
+        *((uint64_t *) (str - length)) ^= mask64;
+        length -= 8;
+    }
+    while (length >= 4) {
+        *((uint32_t *) (str - length)) ^= mask32;
+        length -= 4;
+    }
+    while (length >= 2) {
+        *((uint32_t *) (str - length)) ^= mask16;
+        length -= 2;
+    }
+    while (length != 0) {
+        *(str - length) ^= mask8;
+        --length;
+    }
+
+    for (int i = n; i > 0; --i){
+        if (!isLetter((const char *) *(str - i))){
+            *(str - i) ^= 1 << 5;
         }
     }
 }
 
 int main() {
-    char string[] = "chvgBJHJGFGDcvbnjkNBVHGFfcVBNKjhCGbjYFVbnkliHGvbjkg IUYFvug";
-    printf("test: %s\n", string);
-    changeCase(string, sizeof(string));
+    char string[] = "23456789chvg8BJHJGFGDcvb^%njkNBVHGFfcVBNKjhCG$%^&^%$#$^%$#@$%^%$%^bjYFVbnkl   iH5GvbjkgIUYFvug\0";
+    printf("test:   %s\n", string);
+    changeCase(string, sizeof(string)-1);
     printf("answer: %s\n", string);
 }
