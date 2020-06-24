5e07f903da4ff17026c673bd5539b3484e08a80b
Diana <dianka23@inbox.ru>
2020-06-24 13:26:09 +1000

    fix lab5

--- lab5/main.c
+++ lab5/main.c
@@ -1,13 +1,16 @@
 #include "task1.c"
+#include "task2.c"
 #include "task3.c"
 #include "task4.c"
 #include "task5.c"
+#include "task6.c"
 
 int main(int argc, char *argv[]) {
 //    task1();
 //    task2();
 //    task3(argc, argv);
 //    task4();
-    task5();
+//    task5();
+    task6(argc, argv);
     return 0;
 }
--- lab5/task5.c
+++ lab5/task5.c
@@ -5,19 +5,20 @@
 
 static void myls(const char *name_dir, int flag) {
     DIR *dir;
-    struct dirent *entry; // entry - указатель на структуру dirent
+    struct dirent *p_dir; // p_dir - указатель на структуру dirent
     dir = opendir(name_dir);
     if (!dir) {
         perror("dir opening error!\n");
     }
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type != DT_DIR) {       // если это не директория
-            printf("%s", entry->d_name);
+    while ((p_dir = readdir(dir)) != NULL) {
+        if (p_dir->d_type != DT_DIR) {       // если это не директория
+            printf("%s", p_dir->d_name);
             if (flag) { // если есть -l
                 char fileName[1024];
                 snprintf(fileName, sizeof fileName, "%s%s", name_dir,
-                         entry->d_name); // склеиваем путь
+                         p_dir->d_name); // склеиваем путь
                 struct stat file_stat; // структура для описани файла
+                stat(p_dir->d_name, &file_stat);
                 printf(" (Size: %li, Number link: %lu, Number block: %li, UID: %u, "
                        "GID: %u, Mode: %u)",
                        file_stat.st_size, file_stat.st_nlink, file_stat.st_blocks,
